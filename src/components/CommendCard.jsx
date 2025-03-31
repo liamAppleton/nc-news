ab50a7d2a1527dcc235f5473e4c179533049a21f
Liam Appleton <liamappleton97@outlook.com>
2025-03-31 16:48:21 +0100

    creating basic CommendCard

---
+++ src/components/CommendCard.jsx
@@ -0,0 +1,16 @@
+import Card from 'react-bootstrap/Card';
+import { dateFormatter } from '../../utils/utils';
+
+export const CommendCard = ({ comment }) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Subtitle className="mb-2 text-muted">
+          {comment.author} {dateFormatter(new Date(comment.created_at))}
+        </Card.Subtitle>
+        <Card.Text>{comment.body}</Card.Text>
+        <Card.Text>Votes: {comment.votes}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
