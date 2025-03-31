3036e20cd72c3af155a2f48f33964ed36ca98c2d
Liam Appleton <liamappleton97@outlook.com>
2025-03-31 16:39:42 +0100

    creating link to home from NC news logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">NC News</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          NC News
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
