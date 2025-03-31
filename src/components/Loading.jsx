7123182a23be5172e31d7f1dfce1ba6ed4d939c6
Liam Appleton <liamappleton97@outlook.com>
2025-03-31 16:21:29 +0100

    creating loading component with lottie asset

---
+++ src/components/Loading.jsx
@@ -0,0 +1,6 @@
+import Lottie from 'lottie-react';
+import { loadingSkeletons } from '../assets/loadingSkeletons.json';
+
+export const Loading = () => {
+  return <Lottie animationData={loadingSkeletons}>Loading</Lottie>;
+};
