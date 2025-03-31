bc9d296493419a821ebcc71d2cb42d8ec1cd1dd7
Liam Appleton <liamappleton97@outlook.com>
2025-03-31 16:25:00 +0100

    adding loading to ArticleDisplay

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 import Lottie from 'lottie-react';
-import { loadingSkeletons } from '../assets/loadingSkeletons.json';
+import loadingSkeletons from '../assets/loadingSkeletons.json';
 
 export const Loading = () => {
-  return <Lottie animationData={loadingSkeletons}>Loading</Lottie>;
+  return <Lottie animationData={loadingSkeletons} />;
 };
