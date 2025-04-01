93781c9e549aced8f1af3cc83831ddce99a0b3b6
Liam Appleton <liamappleton97@outlook.com>
2025-04-01 13:51:36 +0100

    Revert "creating UserContext"
    This reverts commit 2e27ea6ca07228a957a1cf2ec39fef4fe1ec67b3.
    
    Removing UserContext as it is not needed yet

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import { ArticleDisplay } from './components/ArticleDisplay';
 import { SingleArticleDisplay } from './components/SingleArticleDisplay';
 import { Header } from './components/Header';
-import { UserProvider } from './contexts/User';
 
 function App() {
   return (
-    <UserProvider>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<ArticleDisplay />} />
@@ -16,7 +15,7 @@ function App() {
           element={<SingleArticleDisplay />}
         />
       </Routes>
-    </UserProvider>
+    </>
   );
 }
 
