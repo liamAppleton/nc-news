ef1a977976c5d11b92607524cf7dc8c11bc1bf40
Liam Appleton <liamappleton97@outlook.com>
2025-04-01 10:47:47 +0100

    adding styling to vote component

--- src/components/Vote.jsx
+++ src/components/Vote.jsx
@@ -3,9 +3,10 @@ import { BiDownvote } from 'react-icons/bi';
 
 export const Vote = () => {
   return (
-    <>
-      <BiUpvote />
-      <BiDownvote />
-    </>
+    <div className="d-inline-flex gap-1 align-items-center border rounded-pill p-1">
+      <BiUpvote size={20} />
+      <p className="fs-6 m-0">vote</p>
+      <BiDownvote size={20} />
+    </div>
   );
 };
