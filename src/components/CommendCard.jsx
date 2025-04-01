585962f816e6fbffb8996fe59b89d6e61b1022b0
Liam Appleton <liamappleton97@outlook.com>
2025-04-01 10:25:52 +0100

    making CommentCard responsive

--- src/components/CommendCard.jsx
+++ src/components/CommendCard.jsx
@@ -3,7 +3,7 @@ import { dateFormatter } from '../../utils/utils';
 
 export const CommendCard = ({ comment }) => {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '100%' }}>
       <Card.Body>
         <Card.Subtitle className="mb-2 text-muted">
           {comment.author} {dateFormatter(new Date(comment.created_at))}
