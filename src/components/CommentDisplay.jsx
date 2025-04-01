fb9bb08684fc8cfe9a334c7cc72df9feb9e9250f
Liam Appleton <liamappleton97@outlook.com>
2025-04-01 16:39:25 +0100

    creating feedback for successful comment post

--- src/components/CommentDisplay.jsx
+++ src/components/CommentDisplay.jsx
@@ -7,6 +7,7 @@ import { getCommentsByArticleId } from '../../api';
 export const CommentDisplay = ({ articleId }) => {
   const [commentsForArticle, setCommentsForArticle] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [commentPosted, setCommentPosted] = useState(false);
 
   useEffect(() => {
     getCommentsByArticleId(articleId).then(({ data: { comments } }) => {
@@ -19,11 +20,19 @@ export const CommentDisplay = ({ articleId }) => {
 
   return (
     <>
-      <div className="mb-3">
+      <div className="mb-3 d-flex justify-content-between position-relative">
         <p>Comments</p>
+        {commentPosted && (
+          <p
+            className="text-success m-0 position-absolute end-0 bottom-0"
+            style={{ fontSize: '0.8rem' }}
+          >
+            Post successful!
+          </p>
+        )}
       </div>
       <div className="mb-3">
-        <AddComment articleId={articleId} />
+        <AddComment articleId={articleId} setCommentPosted={setCommentPosted} />
       </div>
       <div className="row">
         {commentsForArticle.map((comment) => {
