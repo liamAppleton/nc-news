b9a9230fc295aa79dfade7c44ef902462fb68f8c
Liam Appleton <liamappleton97@outlook.com>
2025-04-01 10:35:29 +0100

    creating basic Vote component

---
+++ src/components/Vote.jsx
@@ -0,0 +1,11 @@
+import { BiUpvote } from 'react-icons/bi';
+import { BiDownvote } from 'react-icons/bi';
+
+export const Vote = () => {
+  return (
+    <>
+      <BiUpvote />
+      <BiDownvote />
+    </>
+  );
+};
