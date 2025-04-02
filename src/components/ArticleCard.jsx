c92870e3c4b86d5ee882375b06feace4ffa1cff1
Liam Appleton <liamappleton97@outlook.com>
2025-04-02 11:38:46 +0100

    adding votes and comment counts to article cards

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { FaRegComment } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { Vote } from './Vote';
 import { Loading } from './Loading';
 import { getArticleById } from '../../api';
 import { dateFormatter } from '../../utils/utils';
@@ -27,9 +28,26 @@ export const ArticleCard = ({ articleId }) => {
         <Card.Text>
           {article.author} {dateFormatter(new Date(article.created_at))}
         </Card.Text>
-        <Link to={`/articles/${article.article_id}`}>
-          <Button variant="primary">View article</Button>
-        </Link>
+        <div className="d-flex align-items-center gap-3">
+          <Vote id={article.article_id} votes={article.votes} />
+
+          <div className="d-flex gap-3 align-items-center">
+            <div className="d-inline-flex gap-1 align-items-center border rounded-pill p-1">
+              <Link
+                to={`/articles/${article.article_id}`}
+                className="d-flex align-items-center"
+              >
+                <FaRegComment color={'#808080'} size={20} className="m-0" />
+              </Link>
+              <p
+                className="fs-6 m-0 user-select-none"
+                style={{ cursor: 'default' }}
+              >
+                {article.comment_count}
+              </p>
+            </div>
+          </div>
+        </div>
       </Card.Body>
     </Card>
   );
