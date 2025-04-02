e0753d4f6e7dbcbdb03e5cda5e1051008a38c26f
Liam Appleton <liamappleton97@outlook.com>
2025-04-02 08:46:09 +0100

    changing state type for CommentsContext

--- src/contexts/Comments.jsx
+++ src/contexts/Comments.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const CommentsContext = createContext();
 
 export const CommentsProvider = ({ children }) => {
-  const [commentsUpdated, setCommentsUpdated] = useState(false);
+  const [commentsUpdated, setCommentsUpdated] = useState(null);
 
   return (
     <CommentsContext.Provider value={{ commentsUpdated, setCommentsUpdated }}>
