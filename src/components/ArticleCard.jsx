833491b0d255d22d68685fc14b6fd22c507a974d
Liam Appleton <liamappleton97@outlook.com>
2025-04-02 11:42:21 +0100

    extracting comment count into its own component

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
-import { FaRegComment } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 import { Vote } from './Vote';
+import { CommentCount } from './CommentCount';
 import { Loading } from './Loading';
 import { getArticleById } from '../../api';
 import { dateFormatter } from '../../utils/utils';
@@ -30,23 +29,7 @@ export const ArticleCard = ({ articleId }) => {
         </Card.Text>
         <div className="d-flex align-items-center gap-3">
           <Vote id={article.article_id} votes={article.votes} />
-
-          <div className="d-flex gap-3 align-items-center">
-            <div className="d-inline-flex gap-1 align-items-center border rounded-pill p-1">
-              <Link
-                to={`/articles/${article.article_id}`}
-                className="d-flex align-items-center"
-              >
-                <FaRegComment color={'#808080'} size={20} className="m-0" />
-              </Link>
-              <p
-                className="fs-6 m-0 user-select-none"
-                style={{ cursor: 'default' }}
-              >
-                {article.comment_count}
-              </p>
-            </div>
-          </div>
+          <CommentCount article={article} />
         </div>
       </Card.Body>
     </Card>
