03cb7d03eefab04796bb9c47adbc3549f5f35e0f
Liam Appleton <liamappleton97@outlook.com>
2025-04-03 16:44:17 +0100

    adding link onto logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <Navbar className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand className="d-flex align-items-center gap-2 fs-2">
-          <img
-            alt="NC News logo"
-            src="src/assets/news-favicon.png"
-            width="40"
-            height="40"
-            className="d-inline-block align-top"
-          />{' '}
-          NC News
-        </Navbar.Brand>
+        <Link to="/" className="text-decoration-none">
+          <Navbar.Brand className="d-flex align-items-center gap-2 fs-2">
+            <img
+              alt="NC News logo"
+              src="src/assets/news-favicon.png"
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+            />{' '}
+            NC News
+          </Navbar.Brand>
+        </Link>
       </Container>
     </Navbar>
   );
