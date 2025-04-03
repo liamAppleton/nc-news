ef97e54e62b6751f87294d9b1e1b36e4aa6a571a
Liam Appleton <liamappleton97@outlook.com>
2025-04-03 14:58:38 +0100

    creating ArticlePlaceholder

---
+++ src/components/ArticlePlaceholder.jsx
@@ -0,0 +1,19 @@
+import Card from 'react-bootstrap/Card';
+import Placeholder from 'react-bootstrap/Placeholder';
+
+export const ArticlePlaceholder = () => {
+  return (
+    <Card className="card-width mb-3">
+      <Card.Img variant="top" src="src/assets/placeholder.png" />
+      <Card.Body>
+        <Placeholder as={Card.Title} animation="glow">
+          <Placeholder xs={6} />
+        </Placeholder>
+        <Placeholder as={Card.Text} animation="glow">
+          <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+          <Placeholder xs={6} /> <Placeholder xs={8} />
+        </Placeholder>
+      </Card.Body>
+    </Card>
+  );
+};
