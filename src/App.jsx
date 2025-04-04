d5d11c6778366dc724be52729e4ddf614d862302
Liam Appleton <liamappleton97@outlook.com>
2025-04-04 10:59:59 +0100

    creating HomeContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Route, Routes, useSearchParams } from 'react-router-dom';
 import { ArticleDisplay } from './components/ArticleDisplay';
 import { SingleArticleDisplay } from './components/SingleArticleDisplay';
 import { NavBar } from './components/NavBar';
-import { Header } from './components/Header';
 import { ErrorCard } from './components/ErrorCard';
+import { HomeProvider } from './contexts/Home';
 import { UserProvider } from './contexts/User';
 import { CommentsProvider } from './contexts/Comments';
 import { useState } from 'react';
@@ -14,42 +14,43 @@ function App() {
   const [searchParams, setSearchParams] = useSearchParams();
 
   return (
-    <CommentsProvider>
-      <UserProvider>
-        <Header />
-        <NavBar
-          searchParams={searchParams}
-          setSearchParams={setSearchParams}
-          home={home}
-        />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ArticleDisplay searchParams={searchParams} setHome={setHome} />
-            }
+    <HomeProvider>
+      <CommentsProvider>
+        <UserProvider>
+          <NavBar
+            searchParams={searchParams}
+            setSearchParams={setSearchParams}
+            home={home}
           />
-          <Route
-            path="/topics/:topic"
-            element={
-              <ArticleDisplay searchParams={searchParams} setHome={setHome} />
-            }
-          />
-          <Route
-            path="/articles/:article_id"
-            element={<SingleArticleDisplay home={home} setHome={setHome} />}
-          />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ArticleDisplay searchParams={searchParams} setHome={setHome} />
+              }
+            />
+            <Route
+              path="/topics/:topic"
+              element={
+                <ArticleDisplay searchParams={searchParams} setHome={setHome} />
+              }
+            />
+            <Route
+              path="/articles/:article_id"
+              element={<SingleArticleDisplay home={home} setHome={setHome} />}
+            />
 
-          <Route
-            path="*"
-            to="/"
-            element={
-              <ErrorCard error={{ status: 404, msg: 'path not found' }} />
-            }
-          />
-        </Routes>
-      </UserProvider>
-    </CommentsProvider>
+            <Route
+              path="*"
+              to="/"
+              element={
+                <ErrorCard error={{ status: 404, msg: 'path not found' }} />
+              }
+            />
+          </Routes>
+        </UserProvider>
+      </CommentsProvider>
+    </HomeProvider>
   );
 }
 
