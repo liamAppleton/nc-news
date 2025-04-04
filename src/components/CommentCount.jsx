cfa9a29b8f9f015e3fd3f44ae8866b51f0dab8d1
Liam Appleton <liamappleton97@outlook.com>
2025-04-04 08:13:09 +0100

    removing useless div in CommentCount

--- src/components/CommentCount.jsx
+++ src/components/CommentCount.jsx
@@ -3,23 +3,21 @@ import { Link } from 'react-router-dom';
 
 export const CommentCount = ({ article }) => {
   return (
-    <div className="d-flex gap-3 align-items-center">
-      <div className="d-inline-flex gap-1 align-items-center border rounded-pill p-1">
-        <Link
-          to={`/articles/${article.article_id}`}
-          className="d-flex align-items-center"
-        >
-          <FaRegComment
-            aria-label="view comments"
-            color={'#808080'}
-            size={20}
-            className="m-0 comment-icon"
-          />
-        </Link>
-        <p className="fs-6 m-0 user-select-none" style={{ cursor: 'default' }}>
-          {article.comment_count}
-        </p>
-      </div>
+    <div className="d-inline-flex gap-1 align-items-center border rounded-pill p-1">
+      <Link
+        to={`/articles/${article.article_id}`}
+        className="d-flex align-items-center"
+      >
+        <FaRegComment
+          aria-label="view comments"
+          color={'#808080'}
+          size={20}
+          className="m-0 comment-icon"
+        />
+      </Link>
+      <p className="fs-6 m-0 user-select-none" style={{ cursor: 'default' }}>
+        {article.comment_count}
+      </p>
     </div>
   );
 };
